fff3bb9bdd358e1c7a06904c736a3138cf2af32e
lighthart <dlighthart@mesd.k12.or.us>
2017-08-11 09:34:07 -0700

    Input factory modified

--- Factories/InputControlFactory.php
+++ Factories/InputControlFactory.php
@@ -3,7 +3,6 @@
 namespace Mesd\Jasper\ReportBundle\Factories;
 
 use JasperClient\Interfaces\InputControlAbstractFactory;
-
 use Mesd\Jasper\ReportBundle\Interfaces\AbstractOptionsHandler;
 
 class InputControlFactory implements InputControlAbstractFactory
@@ -12,15 +11,12 @@ class InputControlFactory implements InputControlAbstractFactory
     // CONSTANTS //
     ///////////////
 
-
-    const DEFAULT_GET_IC_FROM = 'Fallback';
+    const DEFAULT_GET_IC_FROM                  = 'Fallback';
     const DEFAULT_INPUT_CONTROL_TYPE_NAMESPACE = 'Mesd\Jasper\ReportBundle\InputControl\\';
-
 
     ///////////////
     // VARIABLES //
     ///////////////
-
 
     /**
      * Where to get the options list from for an input
@@ -40,7 +36,6 @@ class InputControlFactory implements InputControlAbstractFactory
      */
     private $inputControlTypeNamespace;
 
-
     //////////////////
     // BASE METHODS //
     //////////////////
@@ -53,21 +48,19 @@ class InputControlFactory implements InputControlAbstractFactory
      * @param string                  $inputControlTypeNamespace The namespace to get the input control classes from
      */
     public function __construct(
-            AbstractOptionsHandler $optionsHandler,
-            $getICFrom = null,
-            $inputControlTypeNamespace = null
-        ) {
+        AbstractOptionsHandler $optionsHandler,
+                               $getICFrom = null,
+                               $inputControlTypeNamespace = null
+    ) {
         //Set stuff
-        $this->optionsHandler = $optionsHandler;
-        $this->getICFrom = $getICFrom ?: DEFAULT_GET_IC_FROM;
+        $this->optionsHandler            = $optionsHandler;
+        $this->getICFrom                 = $getICFrom ?: DEFAULT_GET_IC_FROM;
         $this->inputControlTypeNamespace = $inputControlTypeNamespace ?: DEFAULT_INPUT_CONTROL_TYPE_NAMESPACE;
     }
-
 
     /////////////////////////
     // IMPLEMENTED METHODS //
     /////////////////////////
-
 
     /**
      * Processes the XML return from the getInputControls call in the client
@@ -77,23 +70,25 @@ class InputControlFactory implements InputControlAbstractFactory
      *
      * @return array                           Collection of the reports input controls
      */
-    public function processInputControlSpecification(\SimpleXMLElement $specification) {
-        $collection = array();
+    public function processInputControlSpecification(\SimpleXMLElement $specification)
+    {
+        $collection = [];
         //Create the input type class for each element
-        foreach($specification->inputControl as $key => $value) {
+        foreach ($specification->inputControl as $key => $value) {
             //Get the class of the type where the class name is the input type in the specified namespace
-            $inputClass = $this->inputControlTypeNamespace . ucfirst($value->type);
+            $inputClass = $this->inputControlTypeNamespace
+            . ucfirst(strtolower($value->type) == "bool" ? "boolean" : $value->type);
             //Try to init the object
             try {
                 $collection[] = new $inputClass(
-                    (string)$value->id,
-                    (string)$value->label,
+                    (string) $value->id,
+                    (string) $value->label,
                     filter_var($value->mandatory, FILTER_VALIDATE_BOOLEAN),
                     filter_var($value->readOnly, FILTER_VALIDATE_BOOLEAN),
-                    (string)$value->type,
-                    (string)$value->uri,
+                    (string) $value->type,
+                    (string) $value->uri,
                     filter_var($value->visible, FILTER_VALIDATE_BOOLEAN),
-                    (object)$value->state,
+                    (object) $value->state,
                     $this->getICFrom,
                     $this->optionsHandler
                 );
